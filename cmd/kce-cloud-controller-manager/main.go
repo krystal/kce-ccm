fef0c1c32f989ee52b5de425359a942077e5c886
Noah Stride <noah@noahstride.co.uk>
2021-05-06 11:45:19 +0100

    docs: add license header and brief explanation

--- cmd/kce-cloud-controller-manager/main.go
+++ cmd/kce-cloud-controller-manager/main.go
@@ -1,4 +1,28 @@
-// Based on https://github.com/kubernetes/kubernetes/blob/master/cmd/cloud-controller-manager/main.go
+/*
+Copyright 2021 Krystal Hosting Ltd.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// The kce-cloud-controller-manager injects the KCE provider and then hands off
+// to the k8s CCM command.
+//
+// The following is based on guidance based on:
+// https://github.com/kubernetes/kubernetes/blob/master/cmd/cloud-controller-manager/main.go
+// You may also find the following interesting:
+// https://github.com/kubernetes/cloud-provider-gcp/blob/master/cmd/cloud-controller-manager/main.go
+// https://github.com/kubernetes/cloud-provider-aws/blob/master/cmd/aws-cloud-controller-manager/main.go
+
 package main
 
 import (
